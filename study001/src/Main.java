e7c8550bc963dc1b05ce5fcd45e9a2e277e8ebf7
sujungkim99 <161662701+SujungKim99@users.noreply.github.com>
2024-02-29 15:57:34 +0900

    file fix

--- study001/src/Main.java
+++ study001/src/Main.java
@@ -1,7 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         //switch case
+       /*
         int a = 2;
+
         switch (a) {
             case 1:
                 System.out.println("A");
@@ -13,5 +15,18 @@ public class Main {
             default:
                 System.out.println("D");
         }
+        */
+
+        //System.out.println("콘솔 화면에 출력");
+
+        System.out.println("화면 출력");
+        System.out.println("화면 + 출력");
+        System.out.println("3.8");
+        System.out.println("3+5");
+        System.out.println("화면"+3);
+        System.out.println("화면"+3+5);
+        System.out.println(3+5+"화면");
+        System.out.println("화면"+(3+5));
     }
+
 }
