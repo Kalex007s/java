0c3af9755e2704a8406239be4d4b0c529072fe0b
Sujung Kim <161662701+SujungKim99@users.noreply.github.com>
2024-04-03 00:10:19 +0900

    0402tset

---
+++ study001/src/StudyGroup/Study0402/HW02.java
@@ -0,0 +1,5 @@
+package StudyGroup.Study0402;
+
+//1부터 50까지의 숫자 중 2의 배수 이면서 5의 배수인 숫자만을 담고 있는 배열 만들고, 배열 안에 있는 값을 출력하시오.
+public class HW02 {
+}
---
+++ study001/src/StudyGroup/Study0402/HW03.java
@@ -0,0 +1,11 @@
+package StudyGroup.Study0402;
+
+
+//아래 정수형 배열에서 짝수인 요소들의 합을 계산하여 출력하는 프로그램을 작성하시오.
+public class HW03 {
+    public static void main(String[] args) {
+        int [] Array = {,1,2,3,4,5,6,7,8,9,10};
+
+
+    }
+}
