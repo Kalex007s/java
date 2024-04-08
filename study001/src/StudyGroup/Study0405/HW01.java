f73ca2759bd035cf4c673bdbe2847b31cdd24157
sujungkim99 <161662701+SujungKim99@users.noreply.github.com>
2024-04-08 22:07:23 +0900

    0408

--- study001/src/StudyGroup/Study0405/HW01.java
+++ study001/src/StudyGroup/Study0405/HW01.java
@@ -5,4 +5,16 @@ package StudyGroup.Study0405;
 //배열 속 숫자들을 작은 순서부터 나열 하시오.
 
 public class HW01 {
+    public static void main(String[] args) {
+        int [] arraySort = {2,50,14,2,74,12,57,42,1,78,63,59,27};
+        for (int i = 0; i < arraySort.length; i++) {
+            for (int j = 0; j < arraySort.length; j++) {
+                if(arraySort[i]>arraySort[j]){
+
+                }
+
+            }
+
+        }
+    }
 }
