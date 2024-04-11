e31449d511a300743097999830efd1348db051e3
Sujung Kim <161662701+SujungKim99@users.noreply.github.com>
2024-04-12 00:49:21 +0900

    1

--- study001/src/StudyGroup/Main.java
+++ study001/src/StudyGroup/Main.java
@@ -2,13 +2,5 @@ package StudyGroup;
 
 public class Main {
     public static void main(String[] args) {
-        int conut = 0;
-        for (int i = 1; i <= 100; i++) {
-            if (i%2==0 && i%5==0){
-                conut++;
-                System.out.print(i+", ");
-            }
-        }System.out.println();
-        System.out.println("총 반복 횟수는 : " + conut);
     }
 }
