c5292c5b08b2b6c6d46d1448f247b147495cf9fe
Sujung Kim <161662701+SujungKim99@users.noreply.github.com>
2024-04-12 00:28:43 +0900

    testEnd

--- study001/src/StudyGroup/Study0411/HW03.java
+++ study001/src/StudyGroup/Study0411/HW03.java
@@ -5,4 +5,18 @@ package StudyGroup.Study0411;
    for문의 총 반복 횟수를 출력하는 코드를 작성하시오.
  */
 public class HW03 {
+    public static void main(String[] args) {
+        int i, count = 0;
+        for (i = 0;i < 100; i++) {
+            if (i%2 == 0){
+                if(i%5==0){
+                    System.out.print(i+" ");
+                    count++;
+                }
+            }
+        }
+        System.out.println(); // 띄어쓰기
+        System.out.println("출력된 숫자들의 갯수 : " + count );
+        System.out.println("for문의 반복 횟수 : "+ i );
+    }
 }
