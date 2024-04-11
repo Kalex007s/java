eae49965ce966942c6325a7dac60e571f1610829
Sujung Kim <161662701+SujungKim99@users.noreply.github.com>
2024-04-12 00:45:49 +0900

    00

--- study001/src/StudyGroup/Study0411/HW03.java
+++ study001/src/StudyGroup/Study0411/HW03.java
@@ -7,7 +7,7 @@ package StudyGroup.Study0411;
 public class HW03 {
     public static void main(String[] args) {
         int i, count = 0;
-        for (i = 0;i < 100; i++) {
+        for (i = 1;i < 100; i++) {
             if (i%2 == 0){
                 if(i%5==0){
                     System.out.print(i+" ");
