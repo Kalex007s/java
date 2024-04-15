26e766d5abd8dc6d079c9c8058169dc15ae6e78c
Sujung Kim <161662701+SujungKim99@users.noreply.github.com>
2024-04-16 02:19:21 +0900

    answer

--- study001/src/StudyGroup/study0415/HW01.java
+++ study001/src/StudyGroup/study0415/HW01.java
@@ -10,13 +10,11 @@ public class HW01 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int userNum = scanner.nextInt();
-
-
-
-
-
-        
-
-
+        for (int i=0; ;i++){
+            if(i==userNum){
+                System.out.println("유저가 입력한 값은 [ " + i + " ] 입니다.");
+                break;
+            }
+        }
     }
 }
