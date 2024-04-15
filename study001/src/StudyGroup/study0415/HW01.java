16c98476c5a3811753bd8289a3101e3265057900
Sujung Kim <161662701+SujungKim99@users.noreply.github.com>
2024-04-15 23:04:21 +0900

    Create HW01.java

---
+++ study001/src/StudyGroup/study0415/HW01.java
@@ -0,0 +1,22 @@
+package StudyGroup.study0415;
+
+
+import java.util.Scanner;
+
+/*
+사용자가 입력한 값을 찾는 코드를 완성하시오.
+ */
+public class HW01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int userNum = scanner.nextInt();
+
+
+
+
+
+        
+
+
+    }
+}
