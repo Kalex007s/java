6bb4259a2d1837131ef2c72ed436ba6729149441
Sujung Kim <161662701+SujungKim99@users.noreply.github.com>
2024-04-17 01:52:16 +0900

    innit

---
+++ study001/src/StudyGroup/Study0416/HW02.java
@@ -0,0 +1,26 @@
+package StudyGroup.Study0416;
+
+
+/*
+
+ 0   2   10
+ 1   4   9
+ 2   6   8
+ 3   8   7
+ 4   10  6
+ 5   12  5
+ 6   14  4
+ 7   16  3
+ 8   18  2
+ 9   20  1
+10   22  0
+
+ */
+
+
+
+public class HW02 {
+    public static void main(String[] args) {
+
+    }
+}
