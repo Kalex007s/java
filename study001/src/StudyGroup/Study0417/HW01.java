b7509229606d7fad5888dc9b5b826aa551574104
Sujung Kim <161662701+SujungKim99@users.noreply.github.com>
2024-04-17 22:48:39 +0900

    test01

---
+++ study001/src/StudyGroup/Study0417/HW01.java
@@ -0,0 +1,6 @@
+package StudyGroup.Study0417;
+
+
+// 사용자로부터 입력받은 n단까지의 구구단을출력하는 코드를 작성하시오.
+public class HW01 {
+}
---
+++ study001/src/StudyGroup/Study0417/HW02.java
@@ -0,0 +1,6 @@
+package StudyGroup.Study0417;
+
+//0부터 n까지의 자연수를 순서대로 더할 때, 어느 항까지 더해야 주어진 값 이상이 되는지를 찾는 프로그램을 작성하세요.
+
+public class HW02 {
+}
