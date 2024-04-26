7e72d68968e59ea1f4bf7af484c240526c77f3a5
Sujung Kim <161662701+SujungKim99@users.noreply.github.com>
2024-04-27 00:09:32 +0900

    HomeWork

---
+++ study001/src/StudyGroup/Study0426/HW01.java
@@ -0,0 +1,4 @@
+package StudyGroup.Study0426;
+//유저가 숫자를 입력하면 그 숫자에 해당 하는 한글을 출력하는 코드를 작성하시오. 범위는 1부터 5까지이며 범위를 초과시 error 를 출력. ex) 유저 1 입력 = 일 출력
+public class HW01 {
+}
---
+++ study001/src/StudyGroup/Study0426/HW03.java
@@ -0,0 +1,8 @@
+package StudyGroup.Study0426;
+//구구단 8단의 코드를 작성하시오.
+//출력 예시 :
+//8 * 1 = 8
+//8 * 2 - 16
+// ...
+public class HW03 {
+}
