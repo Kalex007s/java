d1ba053abdd373e50578784c0dca11e6e30c5f8b
Sujung Kim <161662701+SujungKim99@users.noreply.github.com>
2024-04-28 07:59:04 +0900

    test

--- study001/src/Main.java
+++ study001/src/Main.java
@@ -1,51 +1,55 @@
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        //#1. 비정방행렬의 선언 및 값 대입 방법1
-        int[][] array1 = new int[2][];
-        array1[0]=new int[2];
-        array1[0][0]=1;
-        array1[0][1]=2;
-        array1[1]=new int[3];
-        array1[1][0]=3;
-        array1[1][1]=4;
-        array1[1][2]=5;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("숫자를 1~300중 적어주세요 : ");
+        int input = sc.nextInt();
+        int[] a = new int[100];
+        int[] b = new int[100];
+        int[] c = new int[100];
 
-        System.out.println(array1[0][0]+ " "+array1[0][1]);
-        System.out.println(array1[1][0]+ " "+array1[1][1]+ " "+array1[1][2]);
-        System.out.println();
+        for (int i = 0; i < a.length; i++) {
+            a[i] = a[i]+i;    // a[i] = i+1; i가 0부터 시작하는데, 배열[]에 i를 더하는게 아닌 배열 안에 i+1 한 값을 대입해서 넣어줘야한다.
+            b[i] = 101+i;
+            c[i] = 201+i;
+            if (input == a[i]){
+                System.out.println(i);  //알기 쉽게 s.o.p 안에 ("a배열, "+i+"번째 인덱스") 의 내용을 적어줘야한다.
+            }
+            else if (input == b[i]){
+                System.out.println(i);
+            }
+            else if (input == c[i]){
+                System.out.println(i);
+            }
+        }
+/*      수중이 수정본
+        Scanner sc = new Scanner(System.in);
+        System.out.println("숫자를 1~300중 적어주세요 : ");
+        int input = sc.nextInt();
+        int[] a = new int[100];
+        int[] b = new int[100];
+        int[] c = new int[100];
 
-        int[][] array2 = new int[2][];
-        array2[0]=new int[] {1,2};
-        array2[1]=new int[] {3,4,5};
-
-        System.out.println(array2[0][0]+ " "+array2[0][1]);
-        System.out.println(array2[1][0]+ " "+array2[1][1]+ " "+array2[1][2]);
-        System.out.println();
-
-        //#2. 비정방행렬의 선언 및 값 대입 방법2
-        int[][] array3 = new int[][] {{1,2},{3,4,5}};
-        System.out.println(array3[0][0]+ " "+array3[0][1]);
-        System.out.println(array3[1][0]+ " "+array3[1][1]+ " "+array3[1][2]);
-        System.out.println();
-
-        int[][] array4;
-        array4 = new int[][] {{1,2},{3,4,5}}; //선언 및 값 대입 방법2 = 배열의 선언과 객체 대입 분리 가능
-        System.out.println(array4[0][0]+ " "+array4[0][1]);
-        System.out.println(array4[1][0]+ " "+array4[1][1]+ " "+array4[1][2]);
-        System.out.println();
-
-        //#3. 비정방행렬의 선언 및 값 대입 방법3
-        int[][] array5 = {{1,2},{3,4,5}};
-        System.out.println(array5[0][0]+ " "+array5[0][1]);
-        System.out.println(array5[1][0]+ " "+array5[1][1]+ " "+array5[1][2]);
-        System.out.println();
-
-//		int[][] array6;
-//		array6 = {{1,2},{3,4,5}}; //선언 및 값 대입 방법3 = 배열의 선언과 객체 대입 분리 불가능
+        for (int i = 0; i < a.length; i++) {
+            a[i] = i+1;
+            b[i] = 101+i;
+            c[i] = 201+i;
+            if (input == a[i]){
+                System.out.println("a배열, "+i+"번째 인덱스");
+            }
+            else if (input == b[i]){
+                System.out.println("b배열, "+i+"번째 인덱스");
+            }
+            else if (input == c[i]){
+                System.out.println("c배열, "+i+"번째 인덱스");
+            }
+        }*/
     }
 }
+
+
 
 
 
