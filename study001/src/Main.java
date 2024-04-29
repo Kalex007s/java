4657ec7153acd93374e1b4cf36bd7c46701a48a7
sujungkim99 <161662701+SujungKim99@users.noreply.github.com>
2024-04-29 13:40:50 +0900

    test

--- study001/src/Main.java
+++ study001/src/Main.java
@@ -3,49 +3,43 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("숫자를 1~300중 적어주세요 : ");
-        int input = sc.nextInt();
-        int[] a = new int[100];
-        int[] b = new int[100];
-        int[] c = new int[100];
+        //#1. 2차원 데이터의 배열의 길이
+        int[][] array1 = new int[2][3];
+        System.out.println(array1.length); //2
+        System.out.println(array1[0].length); //3 첫번째 행이 가리키는 1차원 배열의 개수
+        System.out.println(array1[1].length); //3 두번째 행이 가리키는 1차원 배열의 개수
+        System.out.println();
 
-        for (int i = 0; i < a.length; i++) {
-            a[i] = a[i]+i;    // a[i] = i+1; i가 0부터 시작하는데, 배열[]에 i를 더하는게 아닌 배열 안에 i+1 한 값을 대입해서 넣어줘야한다.
-            b[i] = 101+i;
-            c[i] = 201+i;
-            if (input == a[i]){
-                System.out.println(i);  //알기 쉽게 s.o.p 안에 ("a배열, "+i+"번째 인덱스") 의 내용을 적어줘야한다.
+        int[][] array2 = new int[][] {{1,2},{3,4,5}};
+        System.out.println(array2.length); //2
+        System.out.println(array2[0].length); //2 첫번째 행이 가리키는 1차원 배열의 개수
+        System.out.println(array2[1].length); //3 두번째 행이 가리키는 1차원 배열의 개수
+        System.out.println();
+
+        //#2. 2차원 배열의 출력 방법
+        System.out.print(array2[0][0]+" ");
+        System.out.print(array2[0][1]+" ");
+        System.out.println();
+        System.out.print(array2[1][0]+" ");
+        System.out.print(array2[1][1]+" ");
+        System.out.println(array2[1][2]);
+        System.out.println();
+
+        for(int i=0; i<array2.length; i++) {
+            for(int j=0; j<array2[i].length; j++) {
+                System.out.print(array2[i][j]+" "); //1,2,3,4,5
             }
-            else if (input == b[i]){
-                System.out.println(i);
-            }
-            else if (input == c[i]){
-                System.out.println(i);
-            }
+            System.out.println();
         }
-/*      수중이 수정본
-        Scanner sc = new Scanner(System.in);
-        System.out.println("숫자를 1~300중 적어주세요 : ");
-        int input = sc.nextInt();
-        int[] a = new int[100];
-        int[] b = new int[100];
-        int[] c = new int[100];
+        System.out.println();
 
-        for (int i = 0; i < a.length; i++) {
-            a[i] = i+1;
-            b[i] = 101+i;
-            c[i] = 201+i;
-            if (input == a[i]){
-                System.out.println("a배열, "+i+"번째 인덱스");
+        //for(하나의 원소를 꺼냈을때 저장할 변수:집합객체)
+        for(int[] array: array2) {
+            for(int k:array) {
+                System.out.print(k+" ");
             }
-            else if (input == b[i]){
-                System.out.println("b배열, "+i+"번째 인덱스");
-            }
-            else if (input == c[i]){
-                System.out.println("c배열, "+i+"번째 인덱스");
-            }
-        }*/
+            System.out.println();
+        }
     }
 }
 
