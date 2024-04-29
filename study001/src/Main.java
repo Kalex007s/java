16c2491ad002f599bbee9317dcefd758ae39d1c5
Sujung Kim <161662701+SujungKim99@users.noreply.github.com>
2024-04-30 02:05:54 +0900

    iii

--- study001/src/Main.java
+++ study001/src/Main.java
@@ -3,24 +3,16 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        //#2. 문자열 + 기본자료형 // 기본자료형 + 문자열
-        String str3 = "안녕"+1; //안녕1;
-        String str4 = "안녕"+String.valueOf(1); //안녕1
-        String str5 = "안녕"+"1"; //안녕1
+     String str1 = "Hello Java";
+     String str2 = "Goodbye Java";
+     String str3 = "hello java";
 
-        System.out.println(str3);//안녕1
-        System.out.println(str4);//안녕1
-        System.out.println(str5);//안녕1
-        System.out.println();
-
-        //#3. 문자열과 기본자료형 혼용
-        System.out.println(1+"안녕"); //1안녕
-        System.out.println(1+"안녕"+2); //1안녕2
-        System.out.println("안녕"+1+2); //안녕12
-        System.out.println(1+2+"안녕"); //3안녕
+     Boolean bool = str1.equals(str3);
+        System.out.println(bool);
+        Boolean bool2 = str1.equalsIgnoreCase(str3);
+        System.out.println(bool2);
     }
 }
-
 
 
 
--- study001/src/StudyGroup/Study0428/HW01.java
+++ study001/src/StudyGroup/Study0428/HW01.java
@@ -11,63 +11,40 @@ import java.util.Scanner;
 //4. 모든 학생의 각 과목별 총점을 담고 있는 객체를 출력하시오.
 public class HW01 {
     public static void main(String[] args) {
-        int totalKor = 0;
-        int totalMat = 0;
-        int totalSoi = 0;
-        int totalSic = 0;
-
-        //1. + 2.
+        int totalKor = 0, totalMat = 0, totalSoi = 0, totalSic = 0, totalScore = 0;
+        double avgKor = 0.0, avgMat = 0.0, avgSoi = 0.0, avgSic = 0.0;
+        //다차원 배열 만들기
         Scanner scanner = new Scanner(System.in);
-
-        System.out.println("학생들의 국어점수를 입력하시오.");
-        int[] kor = new int[5];
-        for (int i = 0; i < 5; i++) {
-            kor[i] = scanner.nextInt();
-            totalKor = totalKor + kor[i];
-        }
-
-        System.out.println("학생들의 수학점수를 입력하시오.");
-        int[] mat = new int[5];
-        for (int i = 0; i < 5; i++) {
-            mat[i] = scanner.nextInt();
-            totalMat = totalMat + mat[i];
-        }
-
-        System.out.println("학생들의 사회점수를 입력하시오.");
-        int[] soi = new int[5];
-        for (int i = 0; i < 5; i++) {
-            soi[i] = scanner.nextInt();
-            totalSoi = totalSoi + soi[i];
-        }
-
-        System.out.println("학생들의 과학점수를 입력하시오.");
-        int[] sic = new int[5];
-        for (int i = 0; i < 5; i++) {
-            sic[i] = scanner.nextInt();
-            totalSic = totalSic + sic[i];
-        }
-
-        System.out.println("국어 평균 : " + (totalKor/5) + (totalKor%5));
-        System.out.println("수학 평균 : " + (totalMat/5) + (totalMat%5));
-        System.out.println("사회 평균 : " + (totalSoi/5) + (totalSoi%5));
-        System.out.println("과학 평균 : " + (totalSic/5) + (totalSic%5));
-        //3.
-        int totalScore = (totalKor + totalMat + totalSic + totalSoi) /5;
-        System.out.println("반 전체 평균 : "+ totalScore);
-        //4.
-        for (int i = 0; i < kor.length; i++) {
-            if (kor[i] >= totalKor){
-                System.out.println(i+"번 학생은 평균 [이상]입니다.");
-            }else {
-                System.out.println(i+"번 학생은 평균 [이하]입니다.");
+        int [][] gradeArray = new int[5][4];
+        for (int i = 0; i < gradeArray.length; i++) {
+            System.out.println( (i+1) +"번 학생의 점수를 국어,수학,사회,과학 순으로 입력해 주세요.");
+            for (int j = 0; j < gradeArray[i].length; j++) {
+                gradeArray[i][j] = scanner.nextInt();
             }
         }
-        int [] totalScoreByClass = new int[4];
-        totalScoreByClass[0] = totalKor;
-        totalScoreByClass[1] = totalMat;
-        totalScoreByClass[2] = totalSoi;
-        totalScoreByClass[3] = totalSic;
+        // 1번 : 각 과목별로 평균 점수를 구하시오.
+        for (int i = 0; i < gradeArray[0].length; i++) {
+            totalKor = totalKor + gradeArray[i][0];
+            totalMat = totalMat + gradeArray[i][1];
+            totalSoi = totalSoi + gradeArray[i][2];
+            totalSic = totalSic + gradeArray[i][3];
+        }
+        avgKor = (double) totalKor / gradeArray[0].length;
+        avgMat = (double) totalMat / gradeArray[1].length;
+        avgSoi = (double) totalSoi / gradeArray[2].length;
+        avgSic = (double) totalSic / gradeArray[3].length;
 
-        System.out.println("과목 별 총점 : " + Arrays.toString(totalScoreByClass));
+        System.out.println("국어 평균 : " + avgKor);
+        System.out.println("수학 평균 : " + avgMat);
+        System.out.println("사회 평균 : " + avgSoi);
+        System.out.println("과학 평균 : " + avgSic);
+        // 2. 반의 전체 평균을 구하시오.
+        for (int i = 0; i < gradeArray.length; i++) {
+            for (int j = 0; j < gradeArray[i].length; j++) {
+                totalScore = totalScore + gradeArray[i][j];
+            }
+        }
+
+
     }
 }
