dd4ca2d8e41a4dbec80ddfbc629debccd45afaaf
sujungkim99 <161662701+SujungKim99@users.noreply.github.com>
2024-04-29 20:36:39 +0900

    0429

--- study001/src/Main.java
+++ study001/src/Main.java
@@ -3,43 +3,21 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        //#1. 2차원 데이터의 배열의 길이
-        int[][] array1 = new int[2][3];
-        System.out.println(array1.length); //2
-        System.out.println(array1[0].length); //3 첫번째 행이 가리키는 1차원 배열의 개수
-        System.out.println(array1[1].length); //3 두번째 행이 가리키는 1차원 배열의 개수
+        //#2. 문자열 + 기본자료형 // 기본자료형 + 문자열
+        String str3 = "안녕"+1; //안녕1;
+        String str4 = "안녕"+String.valueOf(1); //안녕1
+        String str5 = "안녕"+"1"; //안녕1
+
+        System.out.println(str3);//안녕1
+        System.out.println(str4);//안녕1
+        System.out.println(str5);//안녕1
         System.out.println();
 
-        int[][] array2 = new int[][] {{1,2},{3,4,5}};
-        System.out.println(array2.length); //2
-        System.out.println(array2[0].length); //2 첫번째 행이 가리키는 1차원 배열의 개수
-        System.out.println(array2[1].length); //3 두번째 행이 가리키는 1차원 배열의 개수
-        System.out.println();
-
-        //#2. 2차원 배열의 출력 방법
-        System.out.print(array2[0][0]+" ");
-        System.out.print(array2[0][1]+" ");
-        System.out.println();
-        System.out.print(array2[1][0]+" ");
-        System.out.print(array2[1][1]+" ");
-        System.out.println(array2[1][2]);
-        System.out.println();
-
-        for(int i=0; i<array2.length; i++) {
-            for(int j=0; j<array2[i].length; j++) {
-                System.out.print(array2[i][j]+" "); //1,2,3,4,5
-            }
-            System.out.println();
-        }
-        System.out.println();
-
-        //for(하나의 원소를 꺼냈을때 저장할 변수:집합객체)
-        for(int[] array: array2) {
-            for(int k:array) {
-                System.out.print(k+" ");
-            }
-            System.out.println();
-        }
+        //#3. 문자열과 기본자료형 혼용
+        System.out.println(1+"안녕"); //1안녕
+        System.out.println(1+"안녕"+2); //1안녕2
+        System.out.println("안녕"+1+2); //안녕12
+        System.out.println(1+2+"안녕"); //3안녕
     }
 }
 
