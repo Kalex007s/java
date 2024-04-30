cc32e5fc007cc2e0ca68244c6bd936044573e3f3
sujungkim99 <161662701+SujungKim99@users.noreply.github.com>
2024-04-30 20:46:57 +0900

    answer

--- study001/src/StudyGroup/Study0430/Example02.java
+++ study001/src/StudyGroup/Study0430/Example02.java
@@ -1,4 +1,9 @@
 package StudyGroup.Study0430;
+
+import JavaInterface.A;
+
+import java.util.Arrays;
+
 /* 참고자료 0430(2) 파일 참고해서 코드 작성
 
 1. 참고자료 0430(2) 모습을 출력하시오.
@@ -10,4 +15,30 @@ package StudyGroup.Study0430;
 
  */
 public class Example02 {
+    public static void main(String[] args) {
+        String str1 = "안녕! 나는 '김개똥'입니다";
+        String str2 = "만나서 반갑습니다^^";
+        String str3 = "kgDD123@naver.com";
+        String str4 = "안녕!하세요@앞으로*잘부$탁!드립니다@";
+        // 1번
+        System.out.println(str1);
+        System.out.println(str2);
+        System.out.println(str3);
+        System.out.println(str4);
+        // 2번
+        String str5 = str1.replace("!","하세요");
+        str5 = str5.replace("나는","저는");
+        System.out.println(str5);
+        // 3번
+        String str6 = str1.concat(str2);
+        System.out.println(str6);
+        // 4번
+        String str7 = str3.substring((str3.indexOf("@"))+1);
+        System.out.println(str7);
+        String str8 = str3.substring(0,str3.indexOf("@"));
+        System.out.println(str8.toLowerCase());
+        // 5번
+        String str9 = str4.replaceAll("[^a-zA-Z0-9가-힣]", "");
+        System.out.println(str9);
+    }
 }
