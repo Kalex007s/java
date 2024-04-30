6bd18b697a51680cecd5f30797f526adb9b28860
sujungkim99 <161662701+SujungKim99@users.noreply.github.com>
2024-04-30 13:43:35 +0900

    answer

--- study001/src/StudyGroup/Study0428/HW01.java
+++ study001/src/StudyGroup/Study0428/HW01.java
@@ -12,7 +12,7 @@ import java.util.Scanner;
 public class HW01 {
     public static void main(String[] args) {
         int totalKor = 0, totalMat = 0, totalSoi = 0, totalSic = 0, totalScore = 0;
-        double avgKor = 0.0, avgMat = 0.0, avgSoi = 0.0, avgSic = 0.0;
+        double avgKor, avgMat, avgSoi, avgSic;
         //다차원 배열 만들기
         Scanner scanner = new Scanner(System.in);
         int [][] gradeArray = new int[5][4];
@@ -23,28 +23,37 @@ public class HW01 {
             }
         }
         // 1번 : 각 과목별로 평균 점수를 구하시오.
-        for (int i = 0; i < gradeArray[0].length; i++) {
+        for (int i = 0; i < gradeArray.length; i++) {
             totalKor = totalKor + gradeArray[i][0];
             totalMat = totalMat + gradeArray[i][1];
             totalSoi = totalSoi + gradeArray[i][2];
             totalSic = totalSic + gradeArray[i][3];
         }
-        avgKor = (double) totalKor / gradeArray[0].length;
-        avgMat = (double) totalMat / gradeArray[1].length;
-        avgSoi = (double) totalSoi / gradeArray[2].length;
-        avgSic = (double) totalSic / gradeArray[3].length;
+        avgKor = (double) totalKor / gradeArray.length;
+        avgMat = (double) totalMat / gradeArray.length;
+        avgSoi = (double) totalSoi / gradeArray.length;
+        avgSic = (double) totalSic / gradeArray.length;
 
         System.out.println("국어 평균 : " + avgKor);
         System.out.println("수학 평균 : " + avgMat);
         System.out.println("사회 평균 : " + avgSoi);
         System.out.println("과학 평균 : " + avgSic);
-        // 2. 반의 전체 평균을 구하시오.
+        // 2번 : 반의 전체 평균을 구하시오.
         for (int i = 0; i < gradeArray.length; i++) {
             for (int j = 0; j < gradeArray[i].length; j++) {
                 totalScore = totalScore + gradeArray[i][j];
             }
         }
-
-
+        // 3번 : 학생 5명에 대한 국어 평균 이상 이하 학생을 구분하시오.
+        for (int i = 0; i < gradeArray.length; i++) {
+            if (gradeArray[i][0] > avgKor){
+                System.out.println( i + "번째 학생의 국어 점수는 평균 이상입니다.");
+            } else {
+                System.out.println( i + "번째 학생의 국어 점수는 평균 이하입니다.");
+            }
+        }
+        // 4번 : 모든 학생의 각 과목별 총점을 담고 있는 객체를 출력하시오.
+        int [] totalScoreBySubject = new int[]{totalKor,totalMat,totalSoi,totalSic};
+        System.out.println("A반 학생들의 과목별 총점 : " + Arrays.toString(totalScoreBySubject));
     }
 }
