5e587fc522630062fb804f446457fa0221bad0e5
Sujung Kim <161662701+SujungKim99@users.noreply.github.com>
2024-05-05 01:43:51 +0900

    test

--- study001/src/StudyGroup/Study0502/Example03.java
+++ study001/src/StudyGroup/Study0502/Example03.java
@@ -1,6 +1,19 @@
 package StudyGroup.Study0502;
+
+import java.util.Arrays;
+
 //피보나치 수열을 만드시오.
 //단 길이는 20개로 한함
 public class Example03 {
-
+    public static void main(String[] args) {
+        int [] pi = new int[20];
+        for (int i = 0; i < pi.length; i++) {
+            if(i < 2) {
+                pi[i] = 1;
+            } else {
+                pi[i] = pi[i-1] + pi[i-2];
+            }
+        }
+        System.out.println(Arrays.toString(pi));
+    }
 }
