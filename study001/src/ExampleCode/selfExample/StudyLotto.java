1004407240394113a1cba86436e001a428be5bfb
Sujung Kim <161662701+SujungKim99@users.noreply.github.com>
2024-05-08 02:25:24 +0900

    test

--- study001/src/ExampleCode/selfExample/StudyLotto.java
+++ study001/src/ExampleCode/selfExample/StudyLotto.java
@@ -68,27 +68,28 @@ public class StudyLotto {
 //            System.out.print(charList2[i]);
 //        }
 //        System.out.println();
-/*
+
         // 난수생성법, Random 숫자 만드는 법
-        Random rd = new Random();
-        int randNum = rd.nextInt(10); // 0 부터 입력한 (정수값-1) 범위에서 랜덤
-        System.out.println(randNum);
+//        Random rd = new Random();
+//        int randNum = rd.nextInt(10); // 0 부터 입력한 (정수값-1) 범위에서 랜덤
+//        System.out.println(randNum);
 
         // 9. 1~45까지의 랜덤한 정수를 length가 6인 배열에 넣으시오.
         // 단, 중복 숫자는 허용하지 않음!!
         Random randClass = new Random();
         int[] rottoList = new int[6];
-       for (int i=0; i<rottoList.length; i++) {
-           int temp = randClass.nextInt(45) + 1;
-           for (int j=0; j<i; j++) {
-                if (rottoList[j] == temp) {
-                    temp = randClass.nextInt(45) + 1;
-                    j = -1;
-                }
-            }
-            rottoList[i] = temp;
-        }
-        System.out.println(Arrays.toString(rottoList));
+
+//       for (int i=0; i<rottoList.length; i++) {
+//           int temp = randClass.nextInt(45) + 1;
+//           for (int j=0; j<i; j++) {
+//                if (rottoList[j] == temp) {
+//                    temp = randClass.nextInt(45) + 1;
+//                    j = -1;
+//                }
+//            }
+//            rottoList[i] = temp;
+//        }
+//        System.out.println(Arrays.toString(rottoList));
 
         for (int i=0; i<rottoList.length; i++) {
             rottoList[i] = randClass.nextInt(45) + 1;
@@ -99,7 +100,7 @@ public class StudyLotto {
             }
         }
         System.out.println(Arrays.toString(rottoList));
-*/
+//
 
     }
 }
--- study001/src/StudyGroup/Main.java
+++ study001/src/StudyGroup/Main.java
@@ -1,27 +1,8 @@
 package StudyGroup;
-
-import java.util.Scanner;
+//
 
 public class Main {
     public static void main(String[] args) {
-        // Scanner를 사용하여 사용자 입력을 받습니다.
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("문자열을 입력하세요: ");
 
-        // 사용자로부터 문자열 입력을 받습니다.
-        String input = scanner.nextLine();
-
-        // 입력 받은 문자열을 단어 배열로 분리합니다.
-        // 공백으로 문자열을 분리하여 단어 배열을 만듭니다.
-        String[] words = input.split(" ");
-
-        // 단어 배열을 역순으로 출력합니다.
-        System.out.println("역순으로 출력된 단어:");
-        for (int i = words.length - 1; i >= 0; i--) {
-            System.out.println(words[i]);
-        }
-
-        // Scanner를 닫습니다.
-        scanner.close();
     }
 }
