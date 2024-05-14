bb65fe6844aaf5c9c7e1a372905bf540e4c4aaa4
Sujung Kim <161662701+SujungKim99@users.noreply.github.com>
2024-05-14 22:10:49 +0900

    test

---
+++ study001/src/StudyGroup/Study0514/HW01.java
@@ -0,0 +1,8 @@
+package StudyGroup.Study0514;
+
+public class HW01 {
+    public static void main(String[] args) {
+        //사용자가 값을 입력하면 짝수인지 홀수인지를 구분하는 코드를 작성하시오.
+        //단, 외부클래스 사용.
+    }
+}
---
+++ study001/src/StudyGroup/Study0514/HW02.java
@@ -0,0 +1,9 @@
+package StudyGroup.Study0514;
+
+public class HW02 {
+    public static void main(String[] args) {
+        //구구단 어플을 만드시오.
+        //사용자가 원하는 단수를 입력하면 그에 해당하는 구구단이 출력되야함
+        //단, 외부 클래스 사용
+    }
+}
