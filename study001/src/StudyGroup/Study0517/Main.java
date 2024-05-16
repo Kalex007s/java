0e958284d17a8bbc23dd5cb80761f02931b2200a
Sujung Kim <161662701+SujungKim99@users.noreply.github.com>
2024-05-17 03:26:24 +0900

    test

---
+++ study001/src/StudyGroup/Study0517/Main.java
@@ -0,0 +1,11 @@
+package StudyGroup.Study0517;
+
+public class Main {
+    public static void main(String[] args) {
+        //사용자에게 두 개의 숫자를 입력받아 다음을 수행하는 메서드를 만드시오.
+        //단 외부 클래스를 사용해야 함. -> 새로운 file 이용
+        //1. 두 수를 더하는 메서드를 만드시오.
+        //2. 두 수를 빼는 메서드를 만드시오.
+        //3. 1과 2를 더하는 메서드를 만드시오.
+    }
+}
